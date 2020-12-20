6f20665f9dea4efe79e0aad26c59a1fa457e06dd
vanquish1101 <jba781101@yahoo.com.tw>
2020-12-20 17:56:09 +0800

    feat: add create function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const restaurantList = require('./restaurants.json')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 const Restaurant = require('./models/restaurant')
 const port = 3000
@@ -17,6 +18,7 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
 })
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -37,6 +39,25 @@ app.get('/restaurants/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.get('/restaurant/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/restaurant', (req, res) => {
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter(restaurant => {
@@ -44,6 +65,8 @@ app.get('/search', (req, res) => {
   })
   res.render('index', { restaurants: restaurants, keyword: keyword })
 })
+
+
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
